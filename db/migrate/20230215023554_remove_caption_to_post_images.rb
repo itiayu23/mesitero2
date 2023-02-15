34b177b94d2c52e7ca770817a1c21818964b5f28
EC2 Default User <124165252+itiayu23@users.noreply.github.com>
2023-02-15 07:57:55 +0000

    mesitero4-11

--- db/migrate/20230215023554_remove_caption_to_post_images.rb
+++
@@ -1,4 +0,0 @@
-class RemoveCaptionToPostImages < ActiveRecord::Migration[6.1]
-  def change
-  end
-end
--- db/migrate/20230215023619_remove_shop_name_to_post_images.rb
+++
@@ -1,4 +0,0 @@
-class RemoveShopNameToPostImages < ActiveRecord::Migration[6.1]
-  def change
-  end
-end
