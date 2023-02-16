0f9230b35dc8a7a5b30f56e120f0504a0c9c33c2
EC2 Default User <124165252+itiayu23@users.noreply.github.com>
2023-02-16 08:05:01 +0000

    mesitero4-18

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -2,7 +2,7 @@ class PostImagesController < ApplicationController
   def new
     @post_image = PostImage.new
   end
-  
+
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
@@ -15,16 +15,21 @@ class PostImagesController < ApplicationController
   end
 
   def show
+    @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
   end
-  
+
   def destroy
+    post_image = PostImage.find(params[:id])
+    post_image.destroy
+    redirect_to '/post_images'
   end
-  
+
 private
 
 def post_image_params
-  params.require(:post_image).permit(:shop_name, :caption)
+  params.require(:post_image).permit(:shop_name, :caption, :image)
 end
-  
-  
+
+
 end
